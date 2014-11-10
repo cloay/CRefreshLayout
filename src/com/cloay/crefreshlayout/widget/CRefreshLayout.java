1dafb3a1994520b2d40f55adca3cc43240bcaf65
cloay <shangrody@gmail.com>
2014-11-10 22:40:58 +0800

    .

--- src/com/cloay/crefreshlayout/widget/CRefreshLayout.java
+++ src/com/cloay/crefreshlayout/widget/CRefreshLayout.java
@@ -1,10 +1,15 @@
 package com.cloay.crefreshlayout.widget;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.util.AttributeSet;
-import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 /**
@@ -14,12 +19,25 @@ import android.widget.ListView;
  * @date 2014-11-10 ÏÂÎç6:35:22 
  *
  */
-public class CRefreshLayout extends View implements OnScrollListener{
-	private static final int    PULL_TO_REFRESH      = 1;
-    private static final int    RELEASE_TO_REFRESH   = 2;
-    private static final int    REFRESHING           = 3;
+public class CRefreshLayout extends LinearLayout implements OnScrollListener{
+    
+    private static final float kloadingIndividualAnimationTiming = 0.8f;
+    private static final float kbarDarkAlpha = 0.4f;
+    private static final float kloadingTimingOffset = 0.1f;
+    private static final float kdisappearDuration = 1.2f;
+    private static final float krelativeHeightFactor = 2.f/5.f;
+    
+    private CRefreshLayoutState state;
+    
+    private int dropHeight;
+    private float originalMarginTop;
+    private float disappearProgress;
+    private float internalAnimationFactor;
+    private int horizontalRandomness;
+    private List<BarItem> barItems;
+    
 	private ListView mListView;
-	
+	 
 	public CRefreshLayout(Context context) {
 		super(context);
 	}
@@ -33,10 +51,56 @@ public class CRefreshLayout extends View implements OnScrollListener{
 	}
 
 	
-	public CRefreshLayout(Context context, ListView listView){
-		super(context);
+	public void init(ListView listView, int color, float lineWidth, int dropHeight, 
+			int horizontalRandomness, float internalAnimationFactor){
 		mListView = listView;
+		
+		this.dropHeight = dropHeight;
+		this.horizontalRandomness = horizontalRandomness;
+		this.internalAnimationFactor = internalAnimationFactor;
+		
 		mListView.addView(this);
+		
+		float width = 0;
+		float height = 0;
+		List<Point> startPoints = new ArrayList<Point>();
+		startPoints.add(new Point(100, 100));
+		startPoints.add(new Point(200, 100));
+		startPoints.add(new Point(100, 100));
+		startPoints.add(new Point(100, 200));
+		
+		List<Point> endPoints = new ArrayList<Point>();
+		endPoints.add(new Point(200, 100));
+		endPoints.add(new Point(200, 200));
+		endPoints.add(new Point(100, 200));
+		endPoints.add(new Point(200, 200));
+		
+		for(int i = 0; i < startPoints.size(); i++){
+			Point startP = startPoints.get(i);
+			Point endP = endPoints.get(i);
+			if(startP.x > width) width = startP.x;
+			if(endP.x > width) width = endP.x;
+			if(startP.y > height) height = startP.y;
+			if(endP.y > height) height = endP.y;
+		}
+		this.layout(0, 0, (int)width, (int)height);
+		
+		for(int i = 0; i < startPoints.size(); i++){
+			Point startP = startPoints.get(i);
+			Point endP = endPoints.get(i);
+			BarItem item = new BarItem(getContext(), startP, endP, color, lineWidth);
+			item.setTag(i);
+			item.setBackgroundColor(Color.TRANSPARENT);
+			item.setAlpha(0f);
+			barItems.add(item);
+			this.addView(item);
+			item.setHorizontalRandomness(this.horizontalRandomness, this.dropHeight);
+		}
+		
+		for (BarItem barItem : this.barItems) {
+			barItem.setupFrame();
+	    }
+		
 	}
 	
 	
@@ -53,4 +117,9 @@ public class CRefreshLayout extends View implements OnScrollListener{
 
 	
 	
+	public enum CRefreshLayoutState{
+		CRefreshLayoutStateIdle,
+		CRefreshLayoutStateIdleRefreshing,
+		CRefreshLayoutStateIdleDisappearing
+	}
 }
